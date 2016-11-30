a5a08c92c952ab677e5b15908d486a6183e367fc
Paul Blakey <paulb@mellanox.com>
2016-11-30 16:25:09 +0200

    netdev-tc-offloads: Add ufid to tc/netdev map
    Flows offloaded to tc are identified by priority
    and handle pair while OVS flows are identified by ufid.
    Added a hash map to convert between the two for later
    retrieval and deleting of offloaded flows.
    
    Issue: 895285
    Change-Id: Ieaa9ec1605f41596aaa37b25eecc5094617f0ab4
    Signed-off-by: Paul Blakey <paulb@mellanox.com>
    Reviewed-by: Roi Dayan <roid@mellanox.com>

--- lib/netdev-tc-offloads.c
+++ lib/netdev-tc-offloads.c
@@ -77,6 +77,105 @@ VLOG_DEFINE_THIS_MODULE(netdev_tc_offloads);
 
 static struct vlog_rate_limit rl_err = VLOG_RATE_LIMIT_INIT(9999, 5);
 
+static struct hmap ufid_to_tc = HMAP_INITIALIZER(&ufid_to_tc);
+static struct ovs_mutex ufid_lock = OVS_MUTEX_INITIALIZER;
+
+struct ufid_to_tc_data {
+    struct hmap_node node;
+    ovs_u128 ufid;
+    uint16_t prio;
+    uint32_t handle;
+    struct netdev *netdev;
+};
+
+static bool
+del_ufid_tc_mapping(const ovs_u128 *ufid)
+{
+    size_t hash = hash_bytes(ufid, sizeof *ufid, 0);
+    struct ufid_to_tc_data *data;
+
+    ovs_mutex_lock(&ufid_lock);
+    HMAP_FOR_EACH_WITH_HASH(data, node, hash, &ufid_to_tc) {
+        if (ovs_u128_equals(*ufid, data->ufid)) {
+            break;
+        }
+    }
+    if (data) {
+        hmap_remove(&ufid_to_tc, &data->node);
+        ovs_mutex_unlock(&ufid_lock);
+        netdev_close(data->netdev);
+        free(data);
+        return true;
+    }
+    ovs_mutex_unlock(&ufid_lock);
+    return false;
+}
+
+static ovs_u128 *
+find_ufid(int prio, int handle, struct netdev *netdev)
+{
+    int ifindex = netdev_get_ifindex(netdev);
+    struct ufid_to_tc_data *data;
+
+    ovs_mutex_lock(&ufid_lock);
+    HMAP_FOR_EACH(data, node, &ufid_to_tc) {
+        if (data->prio == prio && data->handle == handle
+            && netdev_get_ifindex(data->netdev) == ifindex) {
+            break;
+        }
+    }
+    ovs_mutex_unlock(&ufid_lock);
+    if (data) {
+        return &data->ufid;
+    }
+    return NULL;
+}
+
+static int
+get_ufid_tc_mapping(const ovs_u128 *ufid, int *prio, struct netdev **netdev)
+{
+    size_t hash = hash_bytes(ufid, sizeof *ufid, 0);
+    struct ufid_to_tc_data *data;
+
+    ovs_mutex_lock(&ufid_lock);
+    HMAP_FOR_EACH_WITH_HASH(data, node, hash, &ufid_to_tc) {
+        if (ovs_u128_equals(*ufid, data->ufid)) {
+            break;
+        }
+    }
+    ovs_mutex_unlock(&ufid_lock);
+    if (data) {
+        if (prio) {
+            *prio = data->prio;
+        }
+        if (netdev) {
+            *netdev = netdev_ref(data->netdev);
+        }
+        return data->handle;
+    }
+    return 0;
+}
+
+static bool
+add_ufid_tc_mapping(const ovs_u128 *ufid, int prio, int handle,
+                    struct netdev *netdev)
+{
+    size_t hash = hash_bytes(ufid, sizeof *ufid, 0);
+    bool replace = del_ufid_tc_mapping(ufid);
+    struct ufid_to_tc_data *new_data = xzalloc(sizeof *new_data);
+
+    new_data->ufid = *ufid;
+    new_data->prio = prio;
+    new_data->handle = handle;
+    new_data->netdev = netdev_ref(netdev);
+
+    ovs_mutex_lock(&ufid_lock);
+    hmap_insert(&ufid_to_tc, &new_data->node, hash);
+    ovs_mutex_unlock(&ufid_lock);
+
+    return replace;
+}
+
 int
 netdev_tc_flow_flush(struct netdev *netdev)
 {
