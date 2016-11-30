e510bbfcb66759946a62f80786f9c5ee65da76b9
Paul Blakey <paulb@mellanox.com>
2016-11-30 17:31:30 +0200

    dpif-netlink: Use netdev flow get api to query a flow
    Search all datapath added netdevs for a given flow
    using netdev flow api and parse it back to dpif flow.
    
    Issue: 895285
    Change-Id: I6cfec6d0ea9a81e7eb691a4c690b498e1a1d1c80
    Signed-off-by: Paul Blakey <paulb@mellanox.com>
    Reviewed-by: Roi Dayan <roid@mellanox.com>
    Reviewed-by: Simon Horman <simon.horman@netronome.com>

--- lib/netdev.c
+++ lib/netdev.c
@@ -2297,6 +2297,24 @@ netdev_ports_flow_del(const void *obj, const ovs_u128 *ufid,
     return ENOENT;
 }
 
+int
+netdev_ports_flow_get(const void *obj, struct match *match,
+                      struct nlattr **actions,
+                      const ovs_u128 *ufid,
+                      struct dpif_flow_stats *stats,
+                      struct ofpbuf *buf)
+{
+    struct port_to_netdev_data *data;
+
+    HMAP_FOR_EACH(data, node, &port_to_netdev) {
+        if (data->obj == obj && !netdev_flow_get(data->netdev, match, actions,
+                                                 ufid, stats, buf)) {
+            return 0;
+        }
+    }
+    return ENOENT;
+}
+
 #ifdef __linux__
 void
 netdev_set_flow_api_enabled(const struct smap *ovs_other_config)
