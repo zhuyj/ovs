b7f05546e982925bab15126829fcae81f79a5477
Paul Blakey <paulb@mellanox.com>
2016-12-04 13:23:50 +0200

    other-config: Add hw-offload switch to control netdev flow offloading
    Add a new configuration option - hw-offload that enables netdev
    flow api. Enabling this option will allow offloading flows
    using netdev implementation instead of the kernel datapath.
    This configuration option defaults to false - disabled.
    
    Change-Id: Ifa2ac9e6057d4f4618e25f355c345125faa79bff
    Signed-off-by: Paul Blakey <paulb@mellanox.com>

--- vswitchd/bridge.c
+++ vswitchd/bridge.c
@@ -2919,6 +2919,8 @@ bridge_run(void)
     cfg = ovsrec_open_vswitch_first(idl);
 
     if (cfg) {
+        netdev_set_flow_api_enabled(smap_get_bool(&cfg->other_config,
+                                                  "hw-offload", false));
         dpdk_init(&cfg->other_config);
     }
 
