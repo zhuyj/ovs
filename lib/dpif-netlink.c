729587c66730f9fcbf604a6a30f999513086a48b
Paul Blakey <paulb@mellanox.com>
2017-05-21 12:31:07 +0300

    dpctl: Indicate if flow is offloaded when dumping flows of all types
    When verbosity is requested on dump-flows (-m) indicate which flows
    are offloaded.
    
    Issue: 1042448
    Change-Id: I9805a491c0cec4380359cdd51a0bdb7e0cb1bfce
    Signed-off-by: Paul Blakey <paulb@mellanox.com>
    Reviewed-by: Roi Dayan <roid@mellanox.com>

--- lib/dpif-netlink.c
+++ lib/dpif-netlink.c
@@ -1573,6 +1573,7 @@ dpif_netlink_flow_to_dpif_flow(struct dpif *dpif, struct dpif_flow *dpif_flow,
                        &dpif_flow->ufid);
     }
     dpif_netlink_flow_get_stats(datapath_flow, &dpif_flow->stats);
+    dpif_flow->offloaded = false;
 }
 
 /* The design is such that all threads are working together on the first dump
@@ -1652,6 +1653,9 @@ dpif_netlink_netdev_match_to_dpif_flow(struct match *match,
     flow->ufid = *ufid;
 
     flow->pmd_id = PMD_ID_NULL;
+
+    flow->offloaded = true;
+
     return 0;
 }
 
