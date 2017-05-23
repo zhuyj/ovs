bcdb3246e6983350542b70695c7968854412d703
Paul Blakey <paulb@mellanox.com>
2017-05-23 11:00:48 +0300

    netdev-tc-offlods: check ip only if we are eth type ip
    Change-Id: I2ccb14fc132b40a0f62e4e537d16111d46020762

--- lib/netdev-tc-offloads.c
+++ lib/netdev-tc-offloads.c
@@ -307,20 +307,27 @@ parse_tc_flower_to_match(struct tc_flower *flower,
         match_set_dl_type(match, key->eth_type);
     }
 
-    if (key->ip_proto && is_ip_any(&match->flow)) {
-        match_set_nw_proto(match, key->ip_proto);
+    if (is_ip_any(&match->flow)) {
+        if (key->ip_proto) {
+            match_set_nw_proto(match, key->ip_proto);
+        }
+
+        match_set_nw_src_masked(match, key->ipv4.ipv4_src, mask->ipv4.ipv4_src);
+        match_set_nw_dst_masked(match, key->ipv4.ipv4_dst, mask->ipv4.ipv4_dst);
+
+        match_set_ipv6_src_masked(match,
+                                  &key->ipv6.ipv6_src, &mask->ipv6.ipv6_src);
+        match_set_ipv6_dst_masked(match,
+                                  &key->ipv6.ipv6_dst, &mask->ipv6.ipv6_dst);
+
+        if (key->ip_proto == IPPROTO_TCP) {
+            match_set_tp_dst_masked(match, key->tcp_dst, mask->tcp_dst);
+            match_set_tp_src_masked(match, key->tcp_src, mask->tcp_src);
+        } else if (key->ip_proto == IPPROTO_UDP) {
+            match_set_tp_dst_masked(match, key->udp_dst, mask->udp_dst);
+            match_set_tp_src_masked(match, key->udp_src, mask->udp_src);
+        }
     }
-
-    match_set_nw_src_masked(match, key->ipv4.ipv4_src, mask->ipv4.ipv4_src);
-    match_set_nw_dst_masked(match, key->ipv4.ipv4_dst, mask->ipv4.ipv4_dst);
-
-    match_set_ipv6_src_masked(match,
-                              &key->ipv6.ipv6_src, &mask->ipv6.ipv6_src);
-    match_set_ipv6_dst_masked(match,
-                              &key->ipv6.ipv6_dst, &mask->ipv6.ipv6_dst);
-
-    match_set_tp_dst_masked(match, key->dst_port, mask->dst_port);
-    match_set_tp_src_masked(match, key->src_port, mask->src_port);
 
     if (flower->tunnel.tunnel) {
         match_set_tun_id(match, flower->tunnel.id);
