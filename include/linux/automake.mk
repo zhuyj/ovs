9261178b2680aad80db14c6bf655f8323ca55c07
Yossi Kuperman <yossiku@mellanox.com>
2018-09-14 00:20:10 +0300

    Add tc_ct.h

--- include/linux/automake.mk
+++ include/linux/automake.mk
@@ -2,4 +2,5 @@ noinst_HEADERS += \
 	include/linux/pkt_cls.h \
 	include/linux/tc_act/tc_pedit.h \
 	include/linux/tc_act/tc_tunnel_key.h \
-	include/linux/tc_act/tc_vlan.h
+	include/linux/tc_act/tc_vlan.h \
+	include/linux/tc_act/tc_ct.h
