ae67524b18710d39833df7c3dafb924bc8eebb4a
Chris Mi <chrism@mellanox.com>
2018-11-12 11:08:38 +0900

    netdev-tc-offloads: Delete ufid tc mapping in the right place
    Currently, the ufid tc mapping is deleted in add_ufid_tc_mapping().
    But if tc_replace_flower() failed, the old ufid tc mapping will not
    be deleted. If another thread adds the same tc mapping successfully,
    then there will be multiple mappings for the same ifindex, handle
    and prio.
    
    Change-Id: I6a480691ed6f12be55c5e1506a6ef51bed37c7f1
    Fixes: 9116730db ("netdev-tc-offloads: Add ufid to tc/netdev map")
    Signed-off-by: Chris Mi <chrism@mellanox.com>
    Reviewed-by: Roi Dayan <roid@mellanox.com>
    Signed-off-by: Simon Horman <simon.horman@netronome.com>

--- lib/netdev-tc-offloads.c
+++ lib/netdev-tc-offloads.c
@@ -166,8 +166,20 @@ del_ufid_tc_mapping(const ovs_u128 *ufid)
     ovs_mutex_unlock(&ufid_lock);
 }
 
-/* Add ufid entry to ufid_tc hashmap.
- * If entry exists already it will be replaced. */
+/* Wrapper function to delete filter and ufid tc mapping */
+static int
+del_filter_and_ufid_mapping(int ifindex, uint32_t chain, int prio, int handle,
+                            uint32_t block_id, const ovs_u128 *ufid)
+{
+    int err;
+
+    err = tc_del_filter(ifindex, chain, prio, handle, block_id);
+    del_ufid_tc_mapping(ufid);
+
+    return err;
+}
+
+/* Add ufid entry to ufid_tc hashmap. */
 static void
 add_ufid_tc_mapping(const ovs_u128 *ufid, uint32_t chain, int prio, int handle,
                     struct netdev *netdev, int ifindex)
@@ -175,8 +187,6 @@ add_ufid_tc_mapping(const ovs_u128 *ufid, uint32_t chain, int prio, int handle,
     size_t ufid_hash = hash_bytes(ufid, sizeof *ufid, 0);
     size_t tc_hash = hash_int(hash_int(hash_int(prio, handle), ifindex), chain);
     struct ufid_tc_data *new_data = xzalloc(sizeof *new_data);
-
-    del_ufid_tc_mapping(ufid);
 
     new_data->ufid = *ufid;
     new_data->chain = chain;
@@ -1459,7 +1469,7 @@ netdev_tc_flow_put(struct netdev *netdev, struct match *match,
     if (handle && prio) {
         VLOG_DBG_RL(&rl, "updating old chain: %d, prio: %d, handle: %d",
                     chain, prio, handle);
-        tc_del_filter(ifindex, chain, prio, handle, block_id);
+        del_filter_and_ufid_mapping(ifindex, chain, prio, handle, block_id, ufid);
     }
 
     if (!prio) {
@@ -1578,8 +1588,7 @@ netdev_tc_flow_del(struct netdev *netdev OVS_UNUSED,
         }
     }
 
-    error = tc_del_filter(ifindex, chain, prio, handle, block_id);
-    del_ufid_tc_mapping(ufid);
+    error = del_filter_and_ufid_mapping(ifindex, chain, prio, handle, block_id, ufid);
 
     netdev_close(dev);
 
