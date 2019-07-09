zhuyj/ovs

commit d5b3672cb5616c8381524286bc82ba42318215db

tc: Add support for NAT
Change-Id: I2d254db80a47db67e12ba2fe49caa3197b192e00

include/linux/tc_act/tc_ct.h
@@ -20,6 +20,15 @@ struct tc_conntrack {
 enum {
 	TCA_CONNTRACK_UNSPEC,
 	TCA_CONNTRACK_PARMS,
+
+	TCA_CONNTRACK_NAT,
+	TCA_CONNTRACK_NAT_SRC,
+	TCA_CONNTRACK_NAT_DST,
+	TCA_CONNTRACK_NAT_IP_MIN,
+	TCA_CONNTRACK_NAT_IP_MAX,
+	TCA_CONNTRACK_NAT_PORT_MIN,
+	TCA_CONNTRACK_NAT_PORT_MAX,
+
 	TCA_CONNTRACK_TM,
 	TCA_CONNTRACK_PAD,
 	__TCA_CONNTRACK_MAX

lib/netdev-tc-offloads.c
@@ -610,6 +610,7 @@ parse_tc_flower_to_match(struct tc_flower *flower,
             }
             break;
             case TC_ACT_CT: {
+                struct ct_nat_info *nat_action_info = &action->ct.nat;
                 size_t ct_offset = nl_msg_start_nested(buf, OVS_ACTION_ATTR_CT);
 
                 if (action->ct.commit)
@@ -633,6 +634,29 @@ parse_tc_flower_to_match(struct tc_flower *flower,
                                                         sizeof *ct_label);
                     ct_label->key = action->ct.label;
                     ct_label->mask = action->ct.label_mask;
+                }
+
+                if (nat_action_info->nat_action) {
+                    size_t nat_offset = nl_msg_start_nested(buf, OVS_CT_ATTR_NAT);
+
+                    if (nat_action_info->nat_action & TC_NAT_ACTION_SRC)
+                        nl_msg_put_flag(buf, OVS_NAT_ATTR_SRC);
+                    if (nat_action_info->nat_action & TC_NAT_ACTION_DST)
+                        nl_msg_put_flag(buf, TC_NAT_ACTION_DST);
+
+                    if (nat_action_info->nat_action & (TC_NAT_ACTION_SRC |
+                                                       TC_NAT_ACTION_DST)) {
+                        nl_msg_put_be32(buf, OVS_NAT_ATTR_IP_MIN, nat_action_info->min_addr.ipv4);
+                        nl_msg_put_be32(buf, OVS_NAT_ATTR_IP_MAX, nat_action_info->max_addr.ipv4);
+                    }
+
+                    if (nat_action_info->nat_action & (TC_NAT_ACTION_SRC_PORT |
+                                                       TC_NAT_ACTION_DST_PORT)) {
+                        nl_msg_put_be16(buf, OVS_NAT_ATTR_PROTO_MIN, nat_action_info->min_port);
+                        nl_msg_put_be16(buf, OVS_NAT_ATTR_PROTO_MAX, nat_action_info->max_port);
+                    }
+
+                    nl_msg_end_nested(buf, nat_offset);
                 }
 
                 nl_msg_end_nested(buf, ct_offset);
@@ -1234,6 +1258,76 @@ netdev_tc_flow_put(struct netdev *netdev, struct match *match,
                         ct_label = nl_attr_get_unspec(ct_attr, sizeof *ct_label);
                         action->ct.label = ct_label->key;
                         action->ct.label_mask = ct_label->mask;
+                    }
+                    break;
+                    case OVS_CT_ATTR_NAT: {
+                        struct ct_nat_info *nat_action_info = &action->ct.nat;
+                        bool ip_min_specified = false;
+                        bool proto_num_min_specified = false;
+                        bool ip_max_specified = false;
+                        bool proto_num_max_specified = false;
+                        const struct nlattr *b_nest;
+                        unsigned int left_nest;
+
+                        /* TODO: that is right? */
+                        nat_action_info->nat_action |= TC_NAT_ACTION;
+
+                        NL_NESTED_FOR_EACH_UNSAFE (b_nest, left_nest, ct_attr) {
+                            enum ovs_nat_attr sub_type_nest = nl_attr_type(b_nest);
+
+                            switch (sub_type_nest) {
+                            case OVS_NAT_ATTR_SRC:
+                            case OVS_NAT_ATTR_DST:
+                                nat_action_info->nat_action |= ((sub_type_nest == OVS_NAT_ATTR_SRC)
+                                                          ? TC_NAT_ACTION_SRC : TC_NAT_ACTION_DST);
+                            break;
+                            case OVS_NAT_ATTR_IP_MIN:
+                                memcpy(&nat_action_info->min_addr,
+                                    nl_attr_get(b_nest),
+                                    nl_attr_get_size(b_nest));
+                                ip_min_specified = true;
+                            case OVS_NAT_ATTR_IP_MAX:
+                                memcpy(&nat_action_info->max_addr,
+                                    nl_attr_get(b_nest),
+                                    nl_attr_get_size(b_nest));
+                                ip_max_specified = true;
+                            break;
+                            case OVS_NAT_ATTR_PROTO_MIN:
+                                nat_action_info->min_port =
+                                    nl_attr_get_u16(b_nest);
+                                proto_num_min_specified = true;
+                            break;
+                            case OVS_NAT_ATTR_PROTO_MAX:
+                                nat_action_info->max_port =
+                                    nl_attr_get_u16(b_nest);
+                                proto_num_max_specified = true;
+                            break;
+                            case OVS_NAT_ATTR_PERSISTENT:
+                            case OVS_NAT_ATTR_PROTO_HASH:
+                            case OVS_NAT_ATTR_PROTO_RANDOM:
+                                VLOG_INFO("TC NAT: persistent, hash and random are not supported yet");
+                            break;
+                            case OVS_NAT_ATTR_UNSPEC:
+                            case __OVS_NAT_ATTR_MAX:
+                                VLOG_ERR("Unknown nat attribute (%d)", sub_type_nest);
+                                return EINVAL;
+                            break;
+                            }
+                        }
+
+                        if (ip_min_specified && !ip_max_specified) {
+                            nat_action_info->max_addr = nat_action_info->min_addr;
+                        }
+                        if (proto_num_min_specified && !proto_num_max_specified) {
+                            nat_action_info->max_port = nat_action_info->min_port;
+                        }
+                        if (proto_num_min_specified || proto_num_max_specified) {
+                            if (nat_action_info->nat_action & TC_NAT_ACTION_SRC) {
+                                nat_action_info->nat_action |= TC_NAT_ACTION_SRC_PORT;
+                            } else if (nat_action_info->nat_action & TC_NAT_ACTION_DST) {
+                                nat_action_info->nat_action |= TC_NAT_ACTION_DST_PORT;
+                            }
+                        }
                     }
                     break;
                 }

lib/odp-util.c
@@ -1677,6 +1677,7 @@ ovs_parse_tnl_push(const char *s, struct ovs_action_push_tnl *data)
     return n;
 }
 
+/* TODO: re-use this? */
 struct ct_nat_params {
     bool snat;
     bool dnat;

lib/tc.c
@@ -839,7 +839,66 @@ static const struct nl_policy conntrack_policy[] = {
     [TCA_CONNTRACK_PARMS] = { .type = NL_A_UNSPEC,
                               .min_len = sizeof(struct tc_conntrack),
                               .optional = false, },
+    [TCA_CONNTRACK_NAT] = { .type = NL_A_FLAG, .optional = true, },
+    [TCA_CONNTRACK_NAT_SRC] = { .type = NL_A_FLAG, .optional = true, },
+    [TCA_CONNTRACK_NAT_DST] = { .type = NL_A_FLAG, .optional = true, },
+    [TCA_CONNTRACK_NAT_IP_MIN] = { .type = NL_A_U32, .optional = true, },
+    [TCA_CONNTRACK_NAT_IP_MAX] = { .type = NL_A_U32, .optional = true, },
+    [TCA_CONNTRACK_NAT_PORT_MIN] = { .type = NL_A_U16, .optional = true, },
+    [TCA_CONNTRACK_NAT_PORT_MAX] = { .type = NL_A_U16, .optional = true, },
 };
+
+static void
+nl_parse_act_conntrack_nat(struct nlattr **tb, struct tc_action *action)
+{
+    struct ct_nat_info *nat_action_info = &action->ct.nat;
+    bool ip_min_specified = false;
+    bool proto_num_min_specified = false;
+    bool ip_max_specified = false;
+    bool proto_num_max_specified = false;
+
+    if (!tb[TCA_CONNTRACK_NAT])
+        return;
+
+    nat_action_info->nat_action |= TC_NAT_ACTION;
+
+    if (tb[TCA_CONNTRACK_NAT_SRC])
+        nat_action_info->nat_action |= TC_NAT_ACTION_SRC;
+    if (tb[TCA_CONNTRACK_NAT_DST])
+        nat_action_info->nat_action |= TC_NAT_ACTION_DST;
+
+    if (tb[TCA_CONNTRACK_NAT_IP_MIN]) {
+        nat_action_info->min_addr.ipv4 = nl_attr_get_u32(tb[TCA_CONNTRACK_NAT_IP_MIN]);
+        ip_min_specified = true;
+    }
+    if (tb[TCA_CONNTRACK_NAT_IP_MAX]) {
+        nat_action_info->max_addr.ipv4 = nl_attr_get_u32(tb[TCA_CONNTRACK_NAT_IP_MAX]);
+        ip_max_specified = true;
+    }
+
+    if (tb[TCA_CONNTRACK_NAT_PORT_MIN]) {
+        nat_action_info->min_port = htons(nl_attr_get_u16(tb[TCA_CONNTRACK_NAT_PORT_MIN]));
+        proto_num_min_specified = true;
+    }
+    if (tb[TCA_CONNTRACK_NAT_PORT_MAX]) {
+        nat_action_info->max_port = htons(nl_attr_get_u16(tb[TCA_CONNTRACK_NAT_PORT_MAX]));
+        proto_num_max_specified = true;
+    }
+
+    if (ip_min_specified && !ip_max_specified) {
+        nat_action_info->max_addr = nat_action_info->min_addr;
+    }
+    if (proto_num_min_specified && !proto_num_max_specified) {
+        nat_action_info->max_port = nat_action_info->min_port;
+    }
+    if (proto_num_min_specified || proto_num_max_specified) {
+        if (nat_action_info->nat_action & TC_NAT_ACTION_SRC) {
+            nat_action_info->nat_action |= TC_NAT_ACTION_SRC_PORT;
+        } else if (nat_action_info->nat_action & TC_NAT_ACTION_DST) {
+            nat_action_info->nat_action |= TC_NAT_ACTION_DST_PORT;
+        }
+    }
+}
 
 static int
 nl_parse_act_conntrack(struct nlattr *options, struct tc_flower *flower)
@@ -865,6 +924,9 @@ nl_parse_act_conntrack(struct nlattr *options, struct tc_flower *flower)
     action->ct.mark_mask = ct->mark_mask;
     memcpy(&action->ct.label, ct->labels, sizeof action->ct.label);
     memcpy(&action->ct.label_mask, ct->labels_mask, sizeof action->ct.label_mask);
+
+    nl_parse_act_conntrack_nat(conntrack_attrs, action);
+
     action->type = TC_ACT_CT;
 
     return 0;
@@ -1377,6 +1439,31 @@ nl_msg_put_act_tunnel_key_set(struct ofpbuf *request, ovs_be64 id,
 }
 
 static void
+nl_msg_put_act_conntrack_nat(struct ofpbuf *request, struct tc_action *action)
+{
+    struct ct_nat_info *p = &action->ct.nat;
+
+    nl_msg_put_flag(request, TCA_CONNTRACK_NAT);
+
+    if (p->nat_action & TC_NAT_ACTION_SRC)
+        nl_msg_put_flag(request, TCA_CONNTRACK_NAT_SRC);
+    if (p->nat_action & TC_NAT_ACTION_DST)
+        nl_msg_put_flag(request, TCA_CONNTRACK_NAT_DST);
+
+    if (p->nat_action & (TC_NAT_ACTION_SRC | TC_NAT_ACTION_DST)) {
+        nl_msg_put_unspec(request, TCA_CONNTRACK_NAT_IP_MIN, &p->min_addr,
+                          sizeof(p->min_addr));
+        nl_msg_put_unspec(request, TCA_CONNTRACK_NAT_IP_MAX, &p->max_addr,
+                          sizeof(p->max_addr));
+    }
+
+    if (p->nat_action & (TC_NAT_ACTION_SRC_PORT | TC_NAT_ACTION_DST_PORT)) {
+        nl_msg_put_be16(request, TCA_CONNTRACK_NAT_PORT_MIN, p->min_port);
+        nl_msg_put_be16(request, TCA_CONNTRACK_NAT_PORT_MAX, p->max_port);
+    }
+}
+
+static void
 nl_msg_put_act_conntrack(struct ofpbuf *request, struct tc_action *action)
 {
     size_t offset;
@@ -1395,6 +1482,9 @@ nl_msg_put_act_conntrack(struct ofpbuf *request, struct tc_action *action)
         memcpy(ct.labels_mask, &action->ct.label_mask, sizeof ct.labels_mask);
 
         nl_msg_put_unspec(request, TCA_CONNTRACK_PARMS, &ct, sizeof ct);
+
+        if (action->ct.nat.nat_action & TC_NAT_ACTION)
+            nl_msg_put_act_conntrack_nat(request, action);
     }
     nl_msg_end_nested(request, offset);
 }

lib/tc.h
@@ -137,6 +137,31 @@ enum tc_action_type {
     TC_ACT_CT,
 };
 
+/* TODO: can we re-use NAT_ACTION_SRC from OVS? */
+enum tc_nat_action {
+    TC_NAT_ACTION_SRC = 1 << 0,
+    TC_NAT_ACTION_SRC_PORT = 1 << 1,
+    TC_NAT_ACTION_DST = 1 << 2,
+    TC_NAT_ACTION_DST_PORT = 1 << 3,
+
+    TC_NAT_ACTION = 1 << 4,
+};
+
+struct ct_addr {
+    union {
+        __be32 ipv4;
+        /* TODO: support IPv6 */
+    };
+};
+
+struct ct_nat_info {
+    uint16_t nat_action;
+    struct ct_addr min_addr;
+    struct ct_addr max_addr;
+    ovs_be16 min_port;
+    ovs_be16 max_port;
+};
+
 struct tc_action {
     union {
         int chain;
@@ -171,6 +196,7 @@ struct tc_action {
             ovs_u128 label;
             ovs_u128 label_mask;
             bool commit;
+            struct ct_nat_info nat;
         } ct;
      };
 

commit c8f903c5b227d05d6361370841617fa23afa2883

tc: Fix conntrack enum order to keep compatibility
Put the following enums at the end instead of in the middle:

	TCA_CONNTRACK_NAT,
	TCA_CONNTRACK_NAT_SRC,
	TCA_CONNTRACK_NAT_DST,
	TCA_CONNTRACK_NAT_IP_MIN,
	TCA_CONNTRACK_NAT_IP_MAX,
	TCA_CONNTRACK_NAT_PORT_MIN,
	TCA_CONNTRACK_NAT_PORT_MAX,

So kernels without nat can still work with updated ovs.

Change-Id: Ida25d7c83862ac668a9b6add5c8a4ff95f70f1c5
Fixes: d5b3672cb561("tc: Add support for NAT")
Signed-off-by: Chris Mi <chrism@mellanox.com>

include/linux/tc_act/tc_ct.h
@@ -22,6 +22,9 @@ enum {
 	TCA_CONNTRACK_UNSPEC,
 	TCA_CONNTRACK_PARMS,
 
+	TCA_CONNTRACK_TM,
+	TCA_CONNTRACK_PAD,
+
 	TCA_CONNTRACK_NAT,
 	TCA_CONNTRACK_NAT_SRC,
 	TCA_CONNTRACK_NAT_DST,
@@ -30,8 +33,6 @@ enum {
 	TCA_CONNTRACK_NAT_PORT_MIN,
 	TCA_CONNTRACK_NAT_PORT_MAX,
 
-	TCA_CONNTRACK_TM,
-	TCA_CONNTRACK_PAD,
 	__TCA_CONNTRACK_MAX
 };
 #define TCA_CONNTRACK_MAX (__TCA_CONNTRACK_MAX - 1)