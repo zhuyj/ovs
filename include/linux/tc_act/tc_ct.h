ecdc56ecb6678bfdcfb33f1a3f5780adda4eff94
Chris Mi <chrism@mellanox.com>
2020-03-26 17:08:10 +0800

    compat: Add CT
    Signed-off-by: Chris Mi <chrism@mellanox.com>

--- include/linux/tc_act/tc_ct.h
+++ include/linux/tc_act/tc_ct.h
@@ -5,6 +5,38 @@
 #include <linux/types.h>
 #include <linux/pkt_cls.h>
 
+#define TCA_ACT_CONNTRACK 18
+
+struct tc_conntrack {
+	tc_gen;
+	__u16 zone;
+	__u32 labels[4];
+	__u32 labels_mask[4];
+	__u32 mark;
+	__u32 mark_mask;
+	bool commit;
+	bool clear;
+};
+
+enum {
+	TCA_CONNTRACK_UNSPEC,
+	TCA_CONNTRACK_PARMS,
+
+	TCA_CONNTRACK_TM,
+	TCA_CONNTRACK_PAD,
+
+	TCA_CONNTRACK_NAT,
+	TCA_CONNTRACK_NAT_SRC,
+	TCA_CONNTRACK_NAT_DST,
+	TCA_CONNTRACK_NAT_IP_MIN,
+	TCA_CONNTRACK_NAT_IP_MAX,
+	TCA_CONNTRACK_NAT_PORT_MIN,
+	TCA_CONNTRACK_NAT_PORT_MAX,
+
+	__TCA_CONNTRACK_MAX
+};
+#define TCA_CONNTRACK_MAX (__TCA_CONNTRACK_MAX - 1)
+
 struct tc_ct {
 	tc_gen;
 };
